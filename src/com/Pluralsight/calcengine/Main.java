344e8305cfe749091e21fab3e8c207748f8f7376
Derek Roman <derek.roman81@gmail.com>
2017-12-16 23:12:45 -0600

    added arrays and switch statement for the engine

--- src/com/Pluralsight/calcengine/Main.java
+++ src/com/Pluralsight/calcengine/Main.java
@@ -3,24 +3,35 @@ package com.Pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 100.0d, val2 = 50.0d, result;
-        char opcode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if (opcode == 'a')
-            result = val1 + val2;
-        else if (opcode == 's')
-            result = val1 - val2;
-        else {
-            if (opcode == 'd')
-                result = val2 != 0.0d ? val1 / val2 : 0.0d;
-            else if (opcode == 'm') {
-                result = val1 * val2;
-            } else {
-                System.out.println("Error - invalid opCode");
-                result = 0.0d;
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0d;
+                    break;
             }
         }
 
-        System.out.println(result);
+        for (double theResult : results) {
+            System.out.println("result = ");
+            System.out.println(theResult);
+        }
     }
 }
